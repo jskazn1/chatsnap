3bdff3bf168b6360044b0ad45b4201b60a8e628f
Jordan Kussmann <jordankussmann@Jordans-MacBook-Pro.local>
2020-01-22 15:29:19 -0800

    added namePicker

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import {db} from './db'
+import NamePicker from './NamePicker.js';
+import { MdSend } from "react-icons/md";
 
 function App() {
+  const [messages, setMessages] = useState([])
+  const [name, setName] = useState('Jordan')
+
+  useEffect(()=>{
+    db.listen({
+      receive: m=> {
+        setMessages(current=> [m, ...current])
+      },
+    })
+  }, [])
+
   return <main>
 
     <header> 
@@ -10,28 +24,42 @@ function App() {
         src="https://www.plantronics.com/etc/designs/plantronics/clientlib-all/img/poly-logo.png" 
       /> 
       Chatter
+      <NamePicker onSave={setName} />
     </header>
 
-    <TextInput onSend={t=> console.log(t)} />
+    <div className={"messages"}>
+      {messages.map((m,i)=> {
+        return <div key={i} className="message-wrap">
+          <div className="message">{m.text}</div>
+        </div>
+      })}
+    </div>
+    <TextInput onSend={(text)=> {
+      db.send({
+        text, name, ts: new Date(),
+      })
+    }} />
 
   </main>
-}
+  }
 
 
-function TextInput(props){
+  function TextInput(props){
   var [text, setText] = useState('')
-  
+
   return <div className="text-input">
     <input value={text} 
       placeholder="Write your message"
       onChange={e=> setText(e.target.value)}
     />
-    <button onClick={()=> {
-      props.onSend(text)
+    <button className="send-logo" onClick={()=> {
+      if(text) {
+        props.onSend(text)
+      }
       setText('')
-    }}>
-      <img className="send-logo" 
-        src="https://cdn1.iconfinder.com/data/icons/outline-17/24/send-512.png" />
+    }}
+    disabled={!text}>
+    {<MdSend />}
     </button>
   </div>
 }
